78151181fabbb09a7455b8e8ec9ec4b83f60c823
Thibault GARCON <thibault.garcon@gmail.com>
2021-11-26 07:35:28 +0100

    CHORE : 1.3.0 version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email="thibault.garcon@gmail.com",
     license="MIT",
     packages=["pyvitemadose"],
-    install_requires=[],
+    install_requires=["requests"],
     zip_safe=False,
     entry_points={
         "console_scripts": [
