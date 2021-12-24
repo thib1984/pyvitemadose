655e7ee5a4eefccee281d8449cf2f58e3e58e264
Thibault GARCON <thibault.garcon@gmail.com>
2021-12-24 09:54:57 +0100

    CHORE: pyinstaller

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pyvitemadose",
-    version="1.0.7",
+    version="1.0.8",
     description="pyvitemadose displays available chronodoses of covid vaccine for your departement in France",
     long_description="The complete description/installation/use/FAQ is available at : https://github.com/thib1984/pyvitemadose#readme",
     url="https://github.com/thib1984/pyvitemadose",
